8cc42564f359fb14edd845ad2e1a3a4d48f78203
Gabberted <43960501+Gabberted@users.noreply.github.com>
2022-04-25 15:44:40 +0200

    app updated

--- app.py
+++ app.py
@@ -4,12 +4,14 @@ from flask import Flask
 from flask import request
 
 
+print("Starting")
 app = Flask(__name__)
+print(f"app {app}")
 
 @app.route('/headers')
 def hello():
-
+    print("hello")
     ua = request.headers.get('user-agent')
     ka = request.headers.get('connection')
-
+    print(f"returning {ua}   {ka}")
     return f'User agent: {ua}; Connection: {ka}'
