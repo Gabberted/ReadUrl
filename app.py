d8d9e4c9d78927fd561170cef7bfbef31029ca04
Gabberted <43960501+Gabberted@users.noreply.github.com>
2022-04-25 15:34:27 +0200

    app.py added

---
+++ app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from flask import Flask
+from flask import request
+
+
+app = Flask(__name__)
+
+@app.route('/headers')
+def hello():
+
+    ua = request.headers.get('user-agent')
+    ka = request.headers.get('connection')
+
+    return f'User agent: {ua}; Connection: {ka}'
