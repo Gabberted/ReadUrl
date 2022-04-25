1d548ee5248b757e9937347229a1d3e3cd603fe3
Gabberted <43960501+Gabberted@users.noreply.github.com>
2022-04-25 17:14:56 +0200

    strHTML

--- readUrl.py
+++ readUrl.py
@@ -58,9 +58,11 @@ def removeHTMLTags(strHTML):
 
 def get_all_seperate_words(url):    
     strHTML= echoHTML(str(url))
+    print(f"strHtml: {strHTML}")
     strHTML=removeHTMLTags(url)
     strHTMLsplit=strHTML.split()
     iLen=len(strHTMLsplit)
+
     print(f"The lenght of strHTMLsplit: {iLen}")
     for strItem in strHTMLsplit:
         print(f"item: {strItem}")
