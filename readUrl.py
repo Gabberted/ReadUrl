ed5b2df09d892de270ee23a87be8189a445da335
Gabberted <43960501+Gabberted@users.noreply.github.com>
2022-04-25 15:22:55 +0200

    webOpen

--- readUrl.py
+++ readUrl.py
@@ -47,8 +47,7 @@ def webOpen(url):
     http = urllib3.PoolManager()
     url = 'http://webcode.me'
     resp = http.request('GET', url)
-    print(resp.text)
-    print(resp.status)
+    print(resp.data.decode('utf-8'))
 
 def main():
     boVerbose=False
